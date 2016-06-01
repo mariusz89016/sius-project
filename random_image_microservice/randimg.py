abde2908528247c06100c1f20fbad441c4cfc97f
Krzysztof Ciombor <krzysztofciombor@gmail.com>
2016-06-01 10:42:50 +0200

    Add configurable parameters for random chance and timeout

--- random_image_microservice/randimg.py
+++ random_image_microservice/randimg.py
@@ -1,11 +1,28 @@
-from flask import Flask, Response
+from flask import Flask, Response, request, abort
+import time
 import urllib.request
+import random
+
 app = Flask(__name__)
 
+DEFAULT_TIMEOUT = 2000 # in milliseconds
+DEFAULT_CHANCE = 0.8 # percent
+
+def get_timeout_in_seconds(timeout_millis):
+    return timeout_millis / 1000
+
 @app.route("/random")
-def hello():
-    image = urllib.request.urlopen("http://source.unsplash.com/random").read()
-    return Response(image, mimetype='image/png')
+def random_image():
+    timeout = int(request.args.get('timeout', DEFAULT_TIMEOUT))
+    random_chance = float(request.args.get('chance', DEFAULT_CHANCE))
+    if random.random() < random_chance:
+        sleep_time = random.uniform(0, get_timeout_in_seconds(timeout))
+        print("Timeout - %s" % sleep_time)
+        time.sleep(sleep_time)
+        image = urllib.request.urlopen("http://source.unsplash.com/random").read()
+        return Response(image, mimetype='image/png')
+    else:
+        return abort(500)
 
 if __name__ == "__main__":
     app.run()
